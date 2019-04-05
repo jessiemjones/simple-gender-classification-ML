98c46f31c9f969e7a181ada1c25aeb160433bb31
Jessie Jones <jessiejonesofficial@gmail.com>
2019-04-05 09:18:44 -0600

    changed output to include probability

--- demo.py
+++ demo.py
@@ -12,31 +12,34 @@ Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male', 'female']
 
 #sample to be tested
-sample = [[174,68,42]]
+sample = [[157,47,38]]
 
 #decision tree
 clf1 = tree.DecisionTreeClassifier()
 clfDT = clf1.fit(X,Y)
 predictionDT = clfDT.predict(sample)
+probabilityDT = clfDT.predict_proba(sample)
 
 #support vector machine
 clf2 = svm.SVC(probability=True)
 clfSVC =clf2.fit(X,Y)
 predicitonSVC = clfSVC.predict(sample)
+probabilitySVC = clfSVC.predict_proba(sample)
 
 #kneighbors classifier for 3 args
 clf3 = KNeighborsClassifier(n_neighbors=3)
 clfN = clf3.fit(X,Y)
 predictionN = clfN.predict(sample)
+probabilityN = clfN.predict_proba(sample)
 
 #gaussian
 clf4 = GaussianProcessClassifier()
 clfG = clf4.fit(X,Y)
 predictionG = clfG.predict(sample)
+probabilityG = clfG.predict_proba(sample)
 
 
-
-print(predictionDT)
-print(predicitonSVC)
-print(predictionN)
-print(predictionG)
+print('predictionDT classified sample data {} to be {} with a probability of {}'.format(sample, predictionDT, probabilityDT))
+print('predicitonSVC classified sample data {} to be {} with a probability of {}'.format(sample, predicitonSVC, probabilitySVC))
+print('predictionN classified sample data {} to be {} with a probability of {}'.format(sample, predictionN, probabilityN))
+print('predictionG classified sample data {} to be {} with a probability of {}'.format(sample, predictionG, probabilityG))
